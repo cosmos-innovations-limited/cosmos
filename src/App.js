8500e23a0f937525999132036a6f1f1614c41c76
Vikas Sharma <vikasisatwork@gmail.com>
2018-12-01 11:26:41 +1300

    Add subscribe feature on the home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import './assets/animate.css';
 import './App.css';
+import Header from './components/Header.js';
+import http from './util/http.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subscriberData: {
+        id: Date.now(),
+        firstName: '',
+        lastName: '',
+        email: ''
+      }
+
+    };
+  }
+
+  emailInputHandler (event) {
+    this.setState({
+      subscriberData: {
+        id: this.state.subscriberData.id,
+        firstName: this.state.subscriberData.firstName,
+        lastName: this.state.subscriberData.lastName,
+        email: event.target.value
+      }
+    })
+  }
+  firstNameInputHandler (event) {
+    this.setState({
+      subscriberData: {
+        id: this.state.subscriberData.id,
+        firstName: event.target.value,
+        lastName: this.state.subscriberData.lastName,
+        email: this.state.subscriberData.email
+      }
+    })
+  }
+  lastNameInputHandler (event) {
+    this.setState({
+      subscriberData: {
+        id: this.state.subscriberData.id,
+        firstName: this.state.subscriberData.firstName,
+        lastName: event.target.value,
+        email: this.state.subscriberData.email
+      }
+    })
+  }
+
+  submitEmailHandler () {
+    http.post(this.state.subscriberData);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Cosmos Innovations  Limited
-          </p>
-          <a
-            className="App-link"
-            href="www.cosmosinnovations.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Solving problems for good.
-          </a>
-        </header>
+        <Header />
+        <div classMame="App-body">
+          <h1 className="display-1">Cosmos</h1>
+          <h3 className="display-5">Project Future: Under Construction</h3>
+          <div className="input-group mb-3 input-subscribe">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input onChange={(event) => this.emailInputHandler(event)} type="text" className="form-control" placeholder="Enter your e-mail ..." aria-label="Email" aria-describedby="email" value={this.state.subscriberData.email}/>
+            <input onChange={(event) => this.firstNameInputHandler(event)} type="text" className="form-control" placeholder="First Name" aria-label="First Name" aria-describedby="First Name" value={this.state.subscriberData.firstName}/>
+            <input onChange={(event) => this.lastNameInputHandler(event)} type="text" className="form-control" placeholder="Last Name" aria-label="Last Name" aria-describedby="Last Name" value={this.state.subscriberData.lastName}/>
+          </div>
+          <button className="btn btn-primary" onClick={() => this.submitEmailHandler()}>Stay updated</button>
+          <p className="display-5">Yes, It is possible!</p>
+        </div>
       </div>
     );
   }
---
+++ src/util/http.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+// import qs from 'qs'
+
+// let config
+let http
+
+const endpoint = 'https://cosmos-d63ab.firebaseio.com/.json';
+
+export default {
+
+  get () {
+    http = axios.create({
+      // baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Cache': 'no-cache',
+        'Authorization': 'none'
+      },
+      credentials: 'same-origin'
+    })
+
+    return http.get(endpoint)
+  },
+
+  post (payLoad) {
+    http = axios.create({
+      // baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Cache': 'no-cache',
+        'Authorization': 'none'
+      },
+      // credentials: 'same-origin'
+    })
+    let data = {
+      'id': payLoad.id,
+      'firstName': payLoad.firstName,
+      'lastName': payLoad.lastName,
+      'email': payLoad.email
+    }
+    return http.post(endpoint, data)
+  }
+
+}
